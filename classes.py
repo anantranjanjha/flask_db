eabe250e83978afbb6b54cf15f35d22886fb999f
anantranjanjha <62759392+anantranjanjha@users.noreply.github.com>
2022-08-28 13:56:23 +0530

    Delete classes.py

--- classes.py
+++
@@ -1,7 +0,0 @@
-class Person:
-  def __init__(self, name, id,department,email_id,phone):
-    self.name = name
-    self.id = id
-    self.department=department
-    self.mail=email_id
-    self.phone=phone
